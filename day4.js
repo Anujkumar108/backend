4510dac807632d4fbab8e92a7e8698e06cad3f5b
Anuj kumar <141645976+Anujkumar108@users.noreply.github.com>
2025-01-16 21:00:12 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,33 @@
+// REST 
+/*
+Representational State Transfer
+REST is an architectural style that defines a set of 
+constraints to be used for creating web services.
+
+*/
+
+
+//CRUD operations
+/*
+Create
+Read
+Update
+Delete
+
+GET retrieves resources.
+POST submits its new data to the server.
+PUT updates existing data.
+DELETE removes data.
+PATCH update existing data partially.
+DELETE removes data.
+
+*/
+
+//CREATING RESTful APIs
+/*
+GET - /posts   - to get data for all posts.   (INDEX) MAIN File
+POST - /posts  - to add a new post.       CREATE
+GET -  /posts/:id - to get one post (using id).   VIEW
+PATCH - /posts/:id - to update specefic post.  UPDATE
+DELETE - /posts/:id - to delete specefic post.  DESTROY
+*/ 
