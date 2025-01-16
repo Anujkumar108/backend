327fe9bcb02f630001c60cf0095010d31f473a3b
Anuj kumar <141645976+Anujkumar108@users.noreply.github.com>
2025-01-16 21:01:57 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -8,7 +8,7 @@ constraints to be used for creating web services.
 
 
 //CRUD operations
-/*
+
 Create
 Read
 Update
@@ -19,15 +19,34 @@ POST submits its new data to the server.
 PUT updates existing data.
 DELETE removes data.
 PATCH update existing data partially.
-DELETE removes data.
+DELETE removes data
 
-*/
-
-//CREATING RESTful APIs
-/*
 GET - /posts   - to get data for all posts.   (INDEX) MAIN File
 POST - /posts  - to add a new post.       CREATE
 GET -  /posts/:id - to get one post (using id).   VIEW
 PATCH - /posts/:id - to update specefic post.  UPDATE
 DELETE - /posts/:id - to delete specefic post.  DESTROY
-*/ 
+
+-> const express = require("express");
+   const app = express();
+
+  const port = 3000;
+  const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static(path.join(__dirname, "public")));
+
+app.get("/",(req, res) => {
+    res.send("server working well! ");
+});
+
+app.listen(port,() => {
+  console.log(`Server is listening on ${port}`);
+});
+
+
+
