031bc9c41e46f1bfe76a7e4348b1e46853e96c56
Anuj kumar <141645976+Anujkumar108@users.noreply.github.com>
2025-01-17 16:44:02 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -132,4 +132,87 @@ h1{
 }
 
 // 4. CREATE AND NEW ROUTE 
+/*Create Route 
+POST       /posts          to add a new post 
+
+2 routes 
+~ Serve the form     GET         /posts/new
+~ Add the new post   POST          /posts 
+*/
+
+// Create a folder name views
+// views/new.ejs
+// file name new.ejs
+   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Create new POST</title>
+</head>
+<body>
+    <form method="post" action="/posts">
+        <input placeholder="enter username" name="username">
+        <br> <br>
+        <textarea placeholder="Write your post" name="content"></textarea>
+        <hr>
+        <button>Submit post</button>
+    </form>
+</body>
+</html>
+
+// main file ~ Index.js :
+
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "anujkumar",
+        content: "I love coding!"
+    },
+    {
+        username: "mayank thakur",
+        content: "consistency is the key to success"
+    },
+    {
+        username: "rudra naresh",
+        content: "har har mahadev!"
+    }
+];
+
+app.get("/posts",(req, res) => {
+    res.render("index.ejs", {posts});
+});
+
+//form path
+app.get("/posts/new",(req, res) => {
+    res.render("new.ejs");
+})
+
+//submission path
+app.post("/posts",(req,res) => {
+   let {username, content} = req.body;
+   posts.push({ username, content });
+   res.send("post request working");
+});
+
+app.listen(port,() => {
+  console.log(`Server is listening on ${port}`);
+});
+
+
+
+
+
 
