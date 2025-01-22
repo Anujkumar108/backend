0a213adbc41c0c0485c167f1a64f842b59868747
Anuj kumar <141645976+Anujkumar108@users.noreply.github.com>
2025-01-22 10:18:00 +0530

    Add files via upload

---
+++ day6.js
@@ -0,0 +1,28 @@
+//NODE WITH SQL
+// in this part we learn how to use backend in database
+// backend ke andar database ko kaise use kar sakte hai
+
+/*
+Using Faker 
+To generate fake data
+
+userId      username      email     password
+
+let getUser = () => {
+    return [
+    faker.datatype.uuid(),
+    faker.internet.userName(),
+    faker.inernet.email(),
+    faker.internet.password(),
+    ];
+};
+
+*/
+
+/*
+MySQL2 Package 
+To connect Node with MySQL
+
+connection.end();   //To close connection
+
+*/ 
